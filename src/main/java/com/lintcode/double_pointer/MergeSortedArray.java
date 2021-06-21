b8a8628b444e1f9b0456c9dd60c08639159de3af
rui.wang@31800263.onaliyun.com <warren.wang@verymro.com>
2021-06-21 10:52:12 +0800

    算法双指针，合并有序数组

---
+++ src/main/java/com/lintcode/double_pointer/MergeSortedArray.java
@@ -0,0 +1,89 @@
+package com.lintcode.double_pointer;
+
+import java.util.Arrays;
+
+/**
+ * @author rui.wang
+ * @version 1.0
+ * @description: 88. 合并两个有序数组
+ * nums1 = [1,2,3] m=3
+ * nums2 = [2,5,6] n=3
+ * nums1[nums1_pointer] <nums2[nums2_pointer]  sorted[0]= nums1[0] nums1_pointer++
+ * nums1[nums1_pointer] <nums2[nums2_pointer] sorted[1]= nums1[1] nums1_pointer++
+ * nums1[nums1_pointer] >nums2[nums2_pointer] sorted[2]= nums2[0] nums2_pointer++
+ * nums1[nums1_pointer] <nums2[nums2_pointer] sorted[3] = nums1[2] nums1_pointer++ 越界
+ * <p>
+ * while(nums1_pointer<=m-1 && nums2_pointer<=n-1) {
+ * if(nums1[nums1_pointer] <nums2[nums2_pointer]){
+ * sorted[sortedIndex] =  nums1[nums1_pointer]
+ * nums1_pointer++;
+ * }else {
+ * sorted[sortedIndex]= nums2[nums2_pointer]
+ * nums2_pointer++
+ * }
+ * }
+ * <p>
+ * 分析：
+ * 1.两个排好序的数组
+ * 2.创建一个新数组 int sorted[] = new int[m+n]
+ * 3.利用双指针移动确定最小值
+ * 1.初始化 nums1_pointer = 0;
+ * 2.初始化 nums2_pointer = 0;
+ * 3.初始化 sortedIndex = 0;
+ * 4.if(nums1[nums1_pointer]<nums2[nums2_pointer]){
+ * sorted[sortedIndex] = nums1[nums1_pointer];
+ * nums1_pointer++;
+ * sortedIndex++;
+ * }
+ * @param:
+ * @description:
+ * @return:
+ * @author: rui.wang
+ * @date: 2021/6/21
+ */
+
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {2, 5, 6};
+        System.out.println(Arrays.toString(mergeSortedArray(nums1, 3, nums2, 3)));
+    }
+
+    public static int[] mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        int length = m + n;
+        if (m <= 0 || n <= 0 || length <= 0) {
+            return null;
+        }
+        int[] sorted = new int[m + n];
+        int nums1_pointer = 0;
+        int nums2_pointer = 0;
+        int sortedIndex = 0;
+        while (nums1_pointer <= m - 1 && nums2_pointer <= n - 1) {
+            if (nums1[nums1_pointer] <= nums2[nums2_pointer]) {
+                sorted[sortedIndex] = nums1[nums1_pointer];
+                nums1_pointer++;
+                sortedIndex++;
+            } else {
+                sorted[sortedIndex] = nums2[nums2_pointer];
+                nums2_pointer++;
+                sortedIndex++;
+            }
+        }
+        //判断nums1有没有数据
+        if (nums1_pointer < m - 1) {
+            for (; nums1_pointer <= m - 1; nums1_pointer++) {
+                sorted[sortedIndex] = nums1[nums1_pointer];
+                sortedIndex++;
+            }
+        }
+        //判断nums2有没有数据
+        if (nums2_pointer < n - 1) {
+            for (; nums2_pointer <= n - 1; nums2_pointer++) {
+                sorted[sortedIndex] = nums2[nums2_pointer];
+                sortedIndex++;
+            }
+        }
+        return sorted;
+    }
+}
